1a88b74add9e7dca7a04c53ba4b375bee510a7e3
FatFrogDev <arizapaul987@gmail.com>
2023-11-20 11:52:06 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; 
+import { RegistrarComponent } from './registrar/registrar.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+{path:'',component :LoginComponent},
+{path:'registrar',component:RegistrarComponent}
+];
+  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+import { RecuperarContrasenaComponent } from './recuperar-contrasena/recuperar-contrasena.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrarComponent,
+    RecuperarContrasenaComponent
   ],
   imports: [
     BrowserModule,
