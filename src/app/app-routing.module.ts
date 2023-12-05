980c6c374d59962d538fe6273ef40d21ef630c95
INGENIERITO8090 <carlosgalindo8090@gmail.com>
2023-12-04 21:53:47 -0500

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component'; 
+import { LoginComponent } from './login/login.component';
 import { RegistrarComponent } from './registrar/registrar.component';
+import {RecuperarContrasenaComponent} from "./recuperar-contrasena/recuperar-contrasena.component";
 
 const routes: Routes = [
 {path:'',component :LoginComponent},
-{path:'registrar',component:RegistrarComponent}
+{path:'registrar',component:RegistrarComponent},
+  {path:'recuperar',component:RecuperarContrasenaComponent}
+
 ];
-  
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
